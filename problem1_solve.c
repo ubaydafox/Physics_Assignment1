e080cc7c3da3c68d3e037e44545237195ce80f1f
Abu Ubayda <139151033+ubaydafox@users.noreply.github.com>
2023-11-12 20:52:51 +0600

    Update problem1_solve.c

--- problem1_solve.c
+++ problem1_solve.c
@@ -1,34 +1,62 @@
 #include<stdio.h>
 #include<math.h>
 
-float m_th_order(double slitdistance, double angletheta, double wavelength){
+#define N 1000000
 
-    return ((slitdistance*1e-6) * sin(angletheta)) / (wavelength*1e-9);
+int main()
+{
+    double l, t[3], a[3]; /// l = Lemda, t = Theta.
+    int m;
+
+    while(1)
+    {
+        printf("Enter the value of Lemda: ");
+        scanf("%lf", &l);
+        
+        if(l < 380 || l > 750)
+        {
+            printf("Out of the range. Please enter a valid number\n");
+        }
+        else
+        {
+            break;
+        }
     }
 
-int main(){
+    printf("Enter the value of m: ");
+    scanf("%d", &m);
 
-    double wavelength, slitdistance, theta, angletheta;
+    printf("Enter the 3 value of a: ");
+    for(int i = 0; i < 3; i++)
+    {
+        double k;
+        scanf("%lf", &k);
+        k /= N;
+        a[i] = k;
+    }
+    
+    l = l / 1000000000;
 
-    printf("Enter Wavelength (nm) : ");
-    scanf("%lf", &wavelength);
+    t[0] = asin((m * l) / a[0]);
+    t[1] = asin((m * l) / a[1]);
+    t[2] = asin((m * l) / a[2]);
 
-    if(350> wavelength || wavelength >750){
-        printf("Out of range.Please enter a valid number :");
-        return 1;
+    if(t[0] >= t[2] && t[0] >= t[1])
+    {
+        printf("%lf would bent the light most.\n", (a[0] * N));
+    }
+    else if(t[2] >= t[1] && t[2] >= t[0])
+    {
+        printf("%lf would bent the light most.\n", (a[2] * N));
+    }
+    else if(t[1] >= t[2] && t[1] >= t[0])
+    {
+        printf("%lf would bent the light most.\n", (a[1] * N));
+    }
+    else
+    {
+        printf("m * lemda / a is greater then 1.\n");
     }
 
-    printf("Enter slit-distance (um) : ");
-    scanf("%lf", &slitdistance);
-
-    printf("Enter angle (degree) : ");
-    scanf("%lf", &theta);
-
-    angletheta= theta *( M_PI / 180.0);
-
-    float order = m_th_order(slitdistance, angletheta, wavelength);
-
-    printf("m-th order : %.1f", floor(order));
-
-return 0;
+    return 0;
 }
